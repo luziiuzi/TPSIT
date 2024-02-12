15b612c7576cbc5be014f92121e972fdd3d44b93
luziiuzi <rainoldilucrezia95@gmail.com>
2024-02-12 12:47:54 +0100

    commit in newbranch

---
+++ com/mycompany/java_socket/Java_socket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_socket;
+
+/**
+ *
+ * @author raino
+ */
+public class Java_socket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
